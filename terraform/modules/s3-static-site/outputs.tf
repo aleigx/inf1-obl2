f1202be69fa016465351a470edbf4b53c52eee6e
Alejandro Iglesias <ale.iglesias.delgado@gmail.com>
2024-05-17 19:20:10 -0300

    Github actions

--- terraform/modules/s3-static-site/outputs.tf
+++ terraform/modules/s3-static-site/outputs.tf
@@ -1,5 +1,9 @@
-
 output "bucket_website_endpoint" {
   description = "The website endpoint of the S3 bucket"
   value       = aws_s3_bucket.react_app_bucket.website_endpoint
+}
+
+output "bucket_name" {
+  description = "The name of the S3 bucket"
+  value       = aws_s3_bucket.react_app_bucket.bucket
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,8 @@ output "bucket_website_endpoint" {
   description = "The website endpoint of the S3 bucket"
   value       = module.react_app_bucket.bucket_website_endpoint
 }
+
+output "bucket_name" {
+  description = "The name of the S3 bucket"
+  value       = module.react_app_bucket.bucket_name
+}
