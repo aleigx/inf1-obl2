0e8792f096089263a15833231502aa8c4ec09fb4
Alejandro Iglesias <ale.iglesias.delgado@gmail.com>
2024-05-26 20:42:01 -0300

    changed lambda to node v18

--- apps/orders-process-func/index.js
+++ apps/orders-process-func/index.js
@@ -1,29 +1,44 @@
-const QUEUE_NAME = "notifications-inf1-ob2";
+const { S3Client, GetObjectCommand } = require('@aws-sdk/client-s3');
+const { SQSClient, GetQueueUrlCommand, SendMessageCommand } = require('@aws-sdk/client-sqs');
+
 const REGION = "us-west-2";
-const AWS = require('aws-sdk');
-AWS.config.update({ region: REGION });
-const s3 = new AWS.S3();
-const sqs = new AWS.SQS();
+const QUEUE_NAME = "notifications-inf1-ob2";
+
+const s3Client = new S3Client({ region: REGION });
+const sqsClient = new SQSClient({ region: REGION });
 
 exports.handler = async (event) => {
     try {
         console.log(event);
         const bucket = event.Records[0].s3.bucket.name;
         const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
-        const params = {
+        const getObjectParams = {
             Bucket: bucket,
             Key: key
         };
-        const data = await s3.getObject(params).promise();
-        const orders = JSON.parse(data.Body.toString());
-        const queueUrl = await sqs.getQueueUrl({ QueueName: QUEUE_NAME }).promise();
-        const sqsParams = {
+        const getObjectCommand = new GetObjectCommand(getObjectParams);
+        const data = await s3Client.send(getObjectCommand);
+        
+        const orders = JSON.parse(await streamToString(data.Body));
+        const getQueueUrlParams = { QueueName: QUEUE_NAME };
+        const getQueueUrlCommand = new GetQueueUrlCommand(getQueueUrlParams);
+        const queueUrl = await sqsClient.send(getQueueUrlCommand);
+        
+        const sendMessageParams = {
             MessageBody: JSON.stringify(orders),
             QueueUrl: queueUrl.QueueUrl
         };
-        await sqs.sendMessage(sqsParams).promise();    
+        const sendMessageCommand = new SendMessageCommand(sendMessageParams);
+        await sqsClient.send(sendMessageCommand);
     } catch (err) {
         console.error(err);
         throw err;
     }
 }
+
+const streamToString = (stream) => new Promise((resolve, reject) => {
+    const chunks = [];
+    stream.on('data', (chunk) => chunks.push(chunk));
+    stream.on('error', reject);
+    stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')));
+});
