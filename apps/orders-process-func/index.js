6add5ce4ea2666fde0f261fe16b24e0be0d039d1
Alejandro Iglesias <ale.iglesias.delgado@gmail.com>
2024-05-26 18:57:38 -0300

    fixes in lambda func

--- apps/orders-process-func/index.js
+++ apps/orders-process-func/index.js
@@ -1,5 +1,7 @@
+const QUEUE_NAME = "notifications-inf1-ob2";
+const REGION = "us-west-2";
 const AWS = require('aws-sdk');
-AWS.config.update({ region: 'us-west-2' });
+AWS.config.update({ region: REGION });
 const s3 = new AWS.S3();
 const sqs = new AWS.SQS();
 
@@ -12,9 +14,10 @@ exports.handler = async (event) => {
     };
     const data = await s3.getObject(params).promise();
     const orders = JSON.parse(data.Body.toString());
+    const queueUrl = await sqs.getQueueUrl({ QueueName: QUEUE_NAME }).promise();
     const sqsParams = {
         MessageBody: JSON.stringify(orders),
-        QueueUrl: process.env.SQS_URL
+        QueueUrl: queueUrl.QueueUrl
     };
     await sqs.sendMessage(sqsParams).promise();
 }
